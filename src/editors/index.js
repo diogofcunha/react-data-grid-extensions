d58f6a83c1771a9708487bfe12be70719ba54619
diogofncunha <diogofncunha@gmail.com>
2017-04-09 18:15:03 +0100

    Updates

--- src/editors/index.js
+++
@@ -1,4 +0,0 @@
-import DateEditor from './DateEditor';
-import PercentageEditor from './PercentageEditor';
-
-export default { PercentageEditor, DateEditor };
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Editors from './editors';
-import Formatters from './formatters';
-import Headers from './headers';
-
-export default { Editors, Formatters, Headers };
