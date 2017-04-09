e9d87d8edebe710800be47a141f4aa5f8f751ae7
Travis CI <nobody@nobody.org>
2017-04-09 19:24:39 +0100

    Tweaks in jarvis

--- scripts/preDeploy.sh
+++ scripts/preDeploy.sh
@@ -1,6 +1,6 @@
 set -o errexit
 
-git config --global user.email "nobody@nobody.org"
+git config --global user.email "diogofncunha@gmail.com"
 git config --global user.name "Travis CI"
 
 npm run build
