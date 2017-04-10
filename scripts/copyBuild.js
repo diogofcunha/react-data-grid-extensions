b76c2b32cee9baaec1c08dc52fe7ff05676b49a0
Diogo <diogofncunha@gmail.com>
2017-04-10 19:53:56 +0100

    Deploy fixes and yarn (#2)
    Deploy fixes and yarn it!

---
+++ scripts/copyBuild.js
@@ -0,0 +1,29 @@
+const fs = require('fs-extra');
+const path = require('path');
+const config = require('../config/paths');
+
+const buildFolder = config.appBuild;
+const rootFolder = path.join(__dirname, '../');
+
+const filesPathsToPresist = [
+   buildFolder,
+   path.join(rootFolder, '.DS_Store'),
+   path.join(rootFolder, '.git')
+];
+
+fs.readdir(rootFolder, (err, files) => {
+  files.forEach(file => {
+     const fullPath = path.join(rootFolder, file);
+     if (filesPathsToPresist.indexOf(fullPath) === -1) {
+      fs.removeSync(fullPath);
+     }
+  });
+
+  fs.copy(buildFolder, rootFolder, err => {
+      if (err) {
+         throw new Error(err)
+      }
+
+      console.log("Build folder copy executed");
+   });
+});
